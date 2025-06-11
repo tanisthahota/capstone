d75d3e77bf8f77d0b5651b2c067e101cf2fafb75
Uday <udaykiran1101@gmail.com>
2025-06-11 18:27:48 +0530

    run.sh added for both

---
+++ microservice/run.sh
@@ -0,0 +1,10 @@
+docker build -t vulnerable-microservice .
+
+
+docker run -d \
+  --name microservice \
+  --network dosnet \
+  --cpus="0.2" \
+  --memory="100m" \
+  -p 5000:5000 \
+  vulnerable-microservice
